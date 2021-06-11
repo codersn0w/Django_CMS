cbb52d90fd78c5bc5a361d9fbfe3cee233f5da08
ThunderRa1n <Podsn0w@gmail.com>
2021-06-11 20:14:18 +0900

    disable email signup

--- private_diary/private_diary/settings_common.py
+++ private_diary/private_diary/settings_common.py
@@ -162,8 +162,8 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_USERNAME_REQUIRED = False
 
 #サインアップにメールアドレス確認を挟むように設定
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_EMAIL_REQUIRED = True
+#ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+#ACCOUNT_EMAIL_REQUIRED = True
 
 #ログイン/ログアウト後の遷移先を設定
 LOGIN_REDIRECT_URL = 'diary:diary_list'
